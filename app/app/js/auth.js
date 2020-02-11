094e5f4ebb0dfaf492b26b27a62710d100d25775
Narvin Singh <38923286+NarvinSingh@users.noreply.github.com>
2020-02-11 16:33:13 -0500

    Refactored into modules, added auth callbacks

---
+++ app/app/js/auth.js
@@ -0,0 +1,131 @@
+'use strict';
+
+const auth = (function () {
+  const defaultClientID = 'NASAPP@AMER.OAUTHAP';
+  const defaultRedirectURI = 'http://localhost';
+  const authCallbacks = [];
+  
+  let clientID;
+  let redirectURI;
+  let authCode = '';
+  let isUnauthorized = true;
+
+  function initAuth() {
+    setClientID(localStorage.getItem('auth-clientID') || defaultClientID);
+    setRedirectURI(localStorage.getItem('auth-redirectURI') || defaultRedirectURI);
+  }
+
+  function getClientID() {
+    return clientID;
+  }
+
+  function setClientID(value) {
+    localStorage.setItem('auth-clientID', value);
+    clientID = value;
+  }
+
+  function getRedirectURI() {
+    return redirectURI;
+  }
+
+  function setRedirectURI(value) {
+    localStorage.setItem('auth-redirectURI', value);
+    redirectURI = value;
+  }
+
+  function getAuthCodeURI() {
+    return `https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${encodeURIComponent(redirectURI)}&client_id=${encodeURIComponent(clientID)}`;
+  }
+
+  function setAuthCode(value) {
+    authCode = value;
+  }
+
+  function getToken() {
+    return localStorage.getItem('auth-accessToken');
+  }
+
+  function getIsUnauthorized() {
+    return isUnauthorized;
+  }
+
+  function setIsUnauthorized(value) {
+    isUnauthorized = !!(value === 'false' ? false : value);
+    authCallbacks.forEach((callback) => callback.call(this, !isUnauthorized));
+  }
+
+  function requestToken() {
+    let uri = 'https://api.tdameritrade.com/v1/oauth2/token';
+    let body = `grant_type=authorization_code&access_type=offline&code=${encodeURIComponent(authCode)}&client_id=${encodeURIComponent(clientID)}&redirect_uri=${encodeURIComponent(redirectURI)}`;
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = processTokenResponse;
+    xhr.open('POST', uri);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(body);
+    log.print(`requestToken body: ${body}`);
+  }
+
+  function refreshToken() {
+    let uri = 'https://api.tdameritrade.com/v1/oauth2/token';
+    let body = `grant_type=refresh_token&refresh_token=${encodeURIComponent(localStorage.refreshToken)}&access_type=offline&client_id=${encodeURIComponent(clientID)}`;
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = processTokenResponse;
+    xhr.open('POST', uri);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(body);
+    log.print(`refreshToken body: ${body}`);
+  }
+
+  function addAuthCallback(callback) {
+    if (!authCallbacks.includes(callback)) {
+      authCallbacks.push(callback);
+    }
+  }
+
+  function removeAuthCallback(callback) {
+    const index = authCallbacks.indexOf(callback);
+
+    if (index !== -1) {
+      authCallbacks.splice(index, 1);
+    }
+  }
+
+  function processTokenResponse() {
+    if (this.readyState == 4 && this.status == 200) {
+      let json = JSON.parse(this.responseText);
+
+      localStorage.setItem('auth-accessToken', json.access_token);
+      localStorage.setItem('auth-refreshToken', json.refresh_token);
+      isUnauthorized = false;
+      authCallbacks.forEach((callback) => callback.call(this, true));
+      log.print(`processTokenResponse: ${this.responseText}`);
+    } else {
+      isUnauthorized = true;
+      authCallbacks.forEach((callback) => callback.call(this, false));
+      log.print(
+        `processTokenResponse: readyState=${this.readyState} status=${this.status}`,
+        log.WARNING
+      );
+    }
+  }
+
+  window.addEventListener('load', initAuth);
+
+  return {
+    getClientID: getClientID,
+    setClientID: setClientID,
+    getRedirectURI: getRedirectURI,
+    setRedirectURI: setRedirectURI,
+    getAuthCodeURI: getAuthCodeURI,
+    setAuthCode: setAuthCode,
+    getToken: getToken,
+    getIsUnauthorized: getIsUnauthorized,
+    setIsUnauthorized: setIsUnauthorized,
+    requestToken: requestToken,
+    refreshToken: refreshToken,
+    addAuthCallback: addAuthCallback,
+    removeAuthCallback: removeAuthCallback
+  }
+}());
---
+++ app/app/js/watchlist-ui.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const watchlistUI = (function () {
+  let listSection;
+
+  function initWatchlistUI() {
+    listSection = document.getElementById('listSection');
+    watchlist.getList().forEach((symbol) => loadSymbol(symbol));
+  }
+
+  function loadSymbol(symbol) {
+    const listRowDivId = `listRow-${symbol}-Div`;
+    
+    let listRowDiv = document.getElementById(listRowDivId);
+
+    if (listRowDiv !== null) {
+      log.print(`loadSymbol: ${symbol} already loaded`, log.WARNING);
+
+      return;
+    }
+
+    listRowDiv = document.createElement('div');
+    listRowDiv.id = `listRow-${symbol}-Div`;
+    listRowDiv.className = 'listRow';
+
+    const underlyingDiv = document.createElement('div');
+    underlyingDiv.id = `underlying-${symbol}-Div`;
+    underlyingDiv.className = 'subPanel underlying';
+    listRowDiv.appendChild(underlyingDiv);
+
+    let elt = document.createElement('button');
+    elt.id = `delete-${symbol}-Button`;
+    elt.className = 'icon delete';
+    elt.addEventListener('click', handleButtonDeleteSymbolClick);
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('div');
+    elt.id = `symbol-${symbol}-Div`;
+    elt.className = 'symbolValue';
+    elt.innerHTML = symbol;
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('label');
+    elt.id = `last-${symbol}-Label`;
+    elt.className = 'last';
+    elt.innerHTML = 'Last';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('div');
+    elt.id = `lastValue-${symbol}-Div`;
+    elt.className = 'lastValue';
+    elt.innerHTML = '--';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('label');
+    elt.id = `target-${symbol}-Label`;
+    elt.className = 'target';
+    elt.innerHTML = 'Target';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('input');
+    elt.id = `targetValue-${symbol}-Input`;
+    elt.className = 'targetValue';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('label');
+    elt.id = `volm-${symbol}-Label`;
+    elt.className = 'volm';
+    elt.innerHTML = 'Volume';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('div');
+    elt.id = `volmValue-${symbol}-Div`;
+    elt.className = 'volmValue';
+    elt.innerHTML = '--';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('label');
+    elt.id = `exp-${symbol}-Label`;
+    elt.className = 'exp';
+    elt.innerHTML = 'Expiration';
+    underlyingDiv.appendChild(elt);
+
+    elt = document.createElement('div');
+    elt.id = `expValue-${symbol}-Div`;
+    elt.className = 'expValue';
+    elt.innerHTML = '--';
+    underlyingDiv.appendChild(elt);
+
+    listSection.appendChild(listRowDiv);
+    log.print(`loadSymbol: ${symbol} loaded`);
+  }
+
+  function unloadSymbol(symbol) {
+    const symbolDiv = document.getElementById(`listRow-${symbol}-Div`);
+
+    if (symbolDiv === null) {
+      log.print(`unloadSymbol: ${symbol} not loaded`, log.WARNING);
+      
+      return;
+    }
+ 
+    listSection.removeChild(symbolDiv);
+  }
+
+  function handleButtonDeleteSymbolClick(event) {
+    const symbol = event.currentTarget.id.split('-')[1];
+
+    unloadSymbol(symbol);
+    watchlist.deleteSymbol(symbol);
+  }
+
+  window.addEventListener('load', initWatchlistUI);
+
+  return {
+    loadSymbol: loadSymbol,
+    unloadSymbol: unloadSymbol
+  }
+}());
